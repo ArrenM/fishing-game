1bd54cad9fdd2b0da7ceceb44c789c9eca1dea1a
ArrenM <arren.meuchel@gmail.com>
2025-09-15 12:18:59 -0500

    reworking menu, still no gameplay

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 namespace SpriteKind {
     export const Transition = SpriteKind.create()
-    export const MenuButton = SpriteKind.create()
-    export const Cursor = SpriteKind.create()
-    export const Selector = SpriteKind.create()
+    export const MenuCursor = SpriteKind.create()
 }
 function mainMenu () {
     scene.setBackgroundImage(assets.image`mainMenuBG`)
@@ -10,70 +8,101 @@ function mainMenu () {
     titleText.setMaxFontHeight(16)
     titleText.left = 4
     titleText.top = 4
-    tempText = textsprite.create("New Game", 0, 1)
-    button1 = makeMenuButton(tempText, "newGame")
-    button1.right = 140
-    button1.top = 30
-    tempText = textsprite.create("Load Game", 0, 1)
-    button2 = makeMenuButton(tempText, "loadGame")
-    button2.right = 140
-    button2.top = 60
-    tempText = textsprite.create("Settings", 0, 1)
-    button3 = makeMenuButton(tempText, "settings")
-    button3.right = 140
-    button3.top = 90
-    promptText1 = textsprite.create("Back", 0, 1)
-    promptText1.setIcon(assets.image`B_Button`)
-    promptText1.setPosition(20, 110)
-    selector = sprites.create(assets.image`selectorRight`, SpriteKind.Selector)
-    selector.setPosition(-100, 0)
-    cursor = sprites.create(assets.image`cursorRight`, SpriteKind.Cursor)
-    controller.moveSprite(cursor, 100, 100)
-    cursor.setStayInScreen(true)
-    cursor.setPosition(70, 30)
+    promptText1 = textsprite.create("Select", 0, 1)
+    promptText1.setIcon(assets.image`A_Button`)
+    promptText1.setPosition(30, 110)
+    menuButtons = [
+    textsprite.create("Start Game", 0, 1),
+    textsprite.create("Enter Password", 0, 1),
+    textsprite.create("Settings", 0, 1),
+    textsprite.create("About", 0, 1)
+    ]
+    cursor = sprites.create(assets.image`cursorRight`, SpriteKind.MenuCursor)
+    selectedButton = 0
+    index = 1
+    for (let value of menuButtons) {
+        value.right = 150
+        value.top = index * 24
+        index += 1
+    }
+    updateCursor()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (cursor) {
+        if (selectedButton == 0) {
+            selectedButton = menuButtons.length - 1
+        } else {
+            selectedButton += -1
+        }
+        updateCursor()
+    }
+})
+function updateCursor () {
+    cursor.right = menuButtons[selectedButton].left
+    cursor.y = menuButtons[selectedButton].y
+    for (let value of menuButtons) {
+        value.setOutline(1, 0)
+    }
+    menuButtons[selectedButton].setOutline(1, 12)
 }
 function callScene (sceenName: string) {
     if (sceenName == "titleScreen") {
         titleScreen()
     } else if (sceenName == "mainMenu") {
         mainMenu()
+    } else {
+        sceneNotFound(sceenName)
     }
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (currentScene == "titleScreen") {
+        scene.cameraShake(2, 100)
+    }
     if (currentScene == "mainMenu") {
-        loadScene("titleScreen", "fade")
+        loadScene("titleScreen", "disolve")
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (currentScene == "titleScreen") {
-        loadScene("mainMenu", "fade")
+        loadScene("mainMenu", "disolve")
+    }
+    if (currentScene == "mainMenu") {
+        if (selectedButton == 0) {
+            startGame()
+        }
+        if (selectedButton == 1) {
+            loadScene("password", "disolve")
+        }
+        if (selectedButton == 2) {
+            loadScene("settings", "disolve")
+        }
+        if (selectedButton == 3) {
+            loadScene("about", "disolve")
+        }
     }
 })
+function startGame () {
+	
+}
 function clearScene () {
     sprites.destroyAllSpritesOfKind(SpriteKind.Text)
-    sprites.destroyAllSpritesOfKind(SpriteKind.MenuButton)
     sprites.destroyAllSpritesOfKind(SpriteKind.Transition)
-    sprites.destroyAllSpritesOfKind(SpriteKind.Cursor)
-    sprites.destroyAllSpritesOfKind(SpriteKind.Selector)
+    sprites.destroyAllSpritesOfKind(SpriteKind.MenuCursor)
     scene.setBackgroundColor(0)
     scene.setBackgroundImage(assets.image`blankBG`)
 }
 function loadScene (sceneName: string, transitionName: string) {
     currentScene = transitionName
-    if (transitionName == "fade") {
-        fade(0, 15, 300)
-    } else if (false) {
-    	
+    if (transitionName == "disolve") {
+        disolve(0, 15, 300)
     }
     clearScene()
-    if (transitionName == "fade") {
+    if (transitionName == "disolve") {
         pause(200)
-    } else if (false) {
-    	
     }
     callScene(sceneName)
-    if ((0 as any) == ("fade" as any)) {
-        fade(15, 0, 300)
+    if (transitionName == "disolve") {
+        disolve(15, 0, 300)
     } else if (transitionName == "start") {
         titleText.y = -100
         titleText.setVelocity(0, 200)
@@ -84,11 +113,15 @@ function loadScene (sceneName: string, transitionName: string) {
     }
     currentScene = sceneName
 }
-sprites.onOverlap(SpriteKind.Cursor, SpriteKind.MenuButton, function (sprite, otherSprite) {
-    selectedButton = otherSprite
-    selector.y = otherSprite.y
-    selector.left = otherSprite.left - 3
-})
+function sceneNotFound (sceneName: string) {
+    titleText = textsprite.create("scene:" + "\"" + sceneName + "\"", 0, 2)
+    titleText.setPosition(80, 60)
+    subtitleText = textsprite.create("not found", 0, 2)
+    subtitleText.setPosition(80, 80)
+    promptText1 = textsprite.create("restart", 0, 1)
+    promptText1.setIcon(assets.image`A_Button`)
+    promptText1.setPosition(80, 100)
+}
 function titleScreen () {
     scene.setBackgroundImage(assets.image`titleScreenBackground`)
     titleText = textsprite.create("Fishing Game", 0, 9)
@@ -101,13 +134,19 @@ function titleScreen () {
     promptText1.setIcon(assets.image`A_Button`)
     promptText1.setPosition(80, 100)
 }
-function fade (colorStart: number, colorEnd: number, time: number) {
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (cursor) {
+        selectedButton = (selectedButton + 1) % menuButtons.length
+        updateCursor()
+    }
+})
+function disolve (colorStart: number, colorEnd: number, time: number) {
     if (transitionSprite) {
         sprites.destroy(transitionSprite)
     }
     transitionSprite = sprites.create(assets.image`blankBG`, SpriteKind.Transition)
     transitionSprite.image.fill(colorStart)
-    for (let index = 0; index < 10; index++) {
+    for (let index2 = 0; index2 < 10; index2++) {
         for (let indexX = 0; indexX <= 160; indexX++) {
             for (let indexY = 0; indexY <= 120; indexY++) {
                 if (Math.percentChance(25)) {
@@ -119,28 +158,13 @@ function fade (colorStart: number, colorEnd: number, time: number) {
     }
     transitionSprite.image.fill(colorEnd)
 }
-function makeMenuButton (textSprite: Sprite, actionName: string) {
-    newButton = sprites.create(textSprite.image, SpriteKind.MenuButton)
-    sprites.destroy(textSprite)
-    sprites.setDataString(newButton, "actionName", actionName)
-    return newButton
-}
-let newButton: Sprite = null
 let transitionSprite: Sprite = null
 let subtitleText: TextSprite = null
-let selectedButton: Sprite = null
 let currentScene = ""
+let index = 0
+let selectedButton = 0
 let cursor: Sprite = null
-let selector: Sprite = null
+let menuButtons: TextSprite[] = []
 let promptText1: TextSprite = null
-let button3: Sprite = null
-let button2: Sprite = null
-let button1: Sprite = null
-let tempText: TextSprite = null
 let titleText: TextSprite = null
 loadScene("titleScreen", "start")
-game.onUpdateInterval(500, function () {
-    if (selectedButton) {
-        selectedButton.startEffect(effects.bubbles, 100)
-    }
-})
